c8031ce0f894d4967eb7825c937e915cbfb8605f
frousseu <frousseu>
2016-11-14 22:28:56 -0500

    rqss maps

--- ebird.R
+++ ebird.R
@@ -4,25 +4,28 @@ library(sp)
 library(rgdal)
 library(rgeos)
 library(quantreg)
+library(FRutils)
+library(scales)
 
 #con<-file("M:/SCF2016_FR/ebird/ebd_CA_relAug-2016/ebd_CA_relAug-2016.txt/ebd_CA_relAug-2016.txt")
 #open(con)
 #d<-read.table(con,skip=5000000,nrow=1,sep="\t")
 
-na<-readOGR(dsn="C:/Users/User/Documents/SCF2016_FR/shapefiles",layer="ne_10m_admin_1_states_provinces",encoding="UTF-8")
+na<-readOGR(dsn="D:/ebird/ebd_CA_relAug-2016.txt",layer="ne_10m_admin_1_states_provinces",encoding="UTF-8")
 
 ll<-"+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 prj<-"+proj=utm +zone=22 +datum=NAD83 +ellps=GRS80"
 laea<-"+proj=laea +lat_0=50 +lon_0=-65"
 
-keep<-c("COMMON NAME","TAXONOMIC ORDER","STATE","LATITUDE","LONGITUDE","OBSERVATION DATE","OBSERVATION COUNT","CATEGORY")
-keepn<-c("sp","taxo","state","lat","lon","date","nb","category")
+keep<-c("COMMON NAME","TAXONOMIC ORDER","STATE","LATITUDE","LONGITUDE","OBSERVATION DATE","OBSERVATION COUNT","CATEGORY","SAMPLING EVENT IDENTIFIER")
+keepn<-c("sp","taxo","state","lat","lon","date","nb","category","sample")
 
-e<-fread("C:/Users/User/Documents/SCF2016_FR/ebird/ebd_CA_relAug-2016.txt/ebd_CA_relAug-2016.txt",encoding = "UTF-8", na.strings = "",header = TRUE, sep = "\t", 
-									colClasses = rep("character", 44),  # For simplicity
-									verbose = TRUE,nrows=10000000,select=keep)
+# 29 409 415 rows in there
+#e<-fread("D:/ebird/ebd_CA_relAug-2016.txt/ebd_CA_relAug-2016.txt",encoding = "UTF-8", na.strings = "",header = TRUE, sep = "\t",colClasses = rep("character", 44),verbose = TRUE,select=keep)#,nrows=30000000) 
 
+#fwrite(e[e$STATE=="Quebec",],"D:/ebird/ebd_CA_relAug-2016.txt/ebirdQC.csv",sep="\t")
 
+e<-fread("D:/ebird/ebd_CA_relAug-2016.txt/ebirdQC.csv",encoding = "UTF-8", na.strings = "",header = TRUE, sep = "\t",verbose = TRUE)
 
 names(e)<-keepn[match(names(e),keep)]
 
@@ -33,23 +36,38 @@ temp<-unique(x[,c("sp","taxo")])
 temp<-temp[order(temp$taxo),]
 x<-x[x$taxo<=2324,]
 x$nb<-as.numeric(x$nb)
+x$nb<-ifelse(is.na(x$nb),1,x$nb)
 x$lon<-as.numeric(x$lon)
 x$lat<-as.numeric(x$lat)
-x$nb<-ifelse(is.na(x$nb),1,x$nb)
 
-xs<-SpatialPoints(matrix(as.numeric(c(x$lon,x$lat)),ncol=2),proj4string=CRS(ll))
-sp<-"Common Goldeneye"
-month<-"02"
+
+sp<-"Northern Shoveler"
+month<-"04"
 m<-x$sp%in%sp & substr(x$date,6,7)%in%month
+xs<-SpatialPoints(matrix(as.numeric(c(x$lon[m],x$lat[m])),ncol=2),proj4string=CRS(ll))
 nb<-x$nb[m]
 lat<-x$lat[m]
 lon<-x$lon[m]
+grid<-hexgrid(xs,width=20000,convex=FALSE)
 
+plot(xs,col="white")
+#plot(grid,add=TRUE,col=alpha("red",0.25))
+#g1<-!apply(gContains(gUnaryUnion(na),grid,byid=TRUE),1,any)
+#grid<-grid[g1,]
 fit<-rqss(nb~qss(cbind(lon,lat),lambda=0.005),tau=0.90)
+#plot(fit,col="red",asp=1.5)
 
-plot(fit,col="red",asp=1.5)
+g<-gBuffer(gConvexHull(xs),width=-0.001)
+o<-over(SpatialPoints(coordinates(grid),CRS(proj4string(grid))),g)
+k<-!is.na(o)
+X<-coordinates(grid)[k,1]
+Y<-coordinates(grid)[k,2]
+
+p<-predict(fit,data.frame(lon=X,lat=Y,stringsAsFactors=FALSE))[,1]
+p<-ifelse(p<0,0,p)
+plot(grid[k,],col=colo.scale(p,c("red","white")),border="white",add=TRUE)
+plot(xs,add=TRUE,pch=1,col=alpha("black",0.005))
 plot(na,add=TRUE)
-
 
 
 xfit<-x[x$sp%in%sp,]
