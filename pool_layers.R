565e94fb72fe0c8a4408c24b693394e66a3d1e43
frousseu <francoisrousseu@hotmail.com>
2016-12-20 17:14:43 -0500

    began month work

--- pool_layers.R
+++ pool_layers.R
@@ -3,15 +3,16 @@
 
 path<-"D:/ebird/kernels"
 
+### subset on certain shapefiles here
 x<-list.files(path,pattern=".shp")
+x<-x[grep("_ebird",x)]
 x<-gsub("\\.shp","",x)
-x<-x[1:18]
+x<-x[seq_along(x)]
 
-
-
+###
 lshp<-list()
 for(i in seq_along(x)){
-	shp<-readOGR(dsn=path,layer=x[i],encoding="UTF-8")
+	shp<-readOGR(dsn=path,layer=x[i],encoding="UTF-8",stringsAsFactors=FALSE)
 	lshp[[i]]<-shp
 }
 
@@ -21,7 +22,16 @@ lshp<-lapply(seq_along(lshp),function(i){
 	spChFIDs(lshp[[i]], paste(row.names(lshp[[i]]),names(lshp)[i],sep="_"))
 })
 
-
 res<-do.call("rbind",lshp)
 
-plot(res[res$id=="k50",],col=alpha("red",0.25))
+plot(res[res$id=="k50",],col=alpha("red",0.15),border=NA)
+
+res$season<-as.character(res$season)
+
+months<-formatC("")
+
+gsub("(.{2})", "\\1 ",res$season)
+
+
+
+
