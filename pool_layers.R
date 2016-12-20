7eac2559d774e6e5679e3a95b546b1984fd77492
frousseu <francoisrousseu@hotmail.com>
2016-12-19 23:08:02 -0500

    shp union

--- pool_layers.R
+++ pool_layers.R
@@ -5,5 +5,23 @@ path<-"D:/ebird/kernels"
 
 x<-list.files(path,pattern=".shp")
 x<-gsub("\\.shp","",x)
+x<-x[1:18]
 
-x<-strsplit(x,"_")
+
+
+lshp<-list()
+for(i in seq_along(x)){
+	shp<-readOGR(dsn=path,layer=x[i],encoding="UTF-8")
+	lshp[[i]]<-shp
+}
+
+names(lshp)<-x
+
+lshp<-lapply(seq_along(lshp),function(i){
+	spChFIDs(lshp[[i]], paste(row.names(lshp[[i]]),names(lshp)[i],sep="_"))
+})
+
+
+res<-do.call("rbind",lshp)
+
+plot(res[res$id=="k50",],col=alpha("red",0.25))
