4c8ed8969d15b3348d51f45fa72e823b37bb4ef1
frousseu <francoisrousseu@hotmail.com>
2016-12-23 15:17:07 -0500

    month table created

--- pool_layers.R
+++ pool_layers.R
@@ -5,9 +5,10 @@ path<-"D:/ebird/kernels"
 
 ### subset on certain shapefiles here
 x<-list.files(path,pattern=".shp")
-x<-x[grep("_ebird",x)]
+#x<-x[grep("_ebird",x)]
 x<-gsub("\\.shp","",x)
 x<-x[seq_along(x)]
+
 
 ###
 lshp<-list()
@@ -24,14 +25,20 @@ lshp<-lapply(seq_along(lshp),function(i){
 
 res<-do.call("rbind",lshp)
 
-plot(res[res$id=="k50",],col=alpha("red",0.15),border=NA)
+
 
 res$season<-as.character(res$season)
 
-months<-formatC("")
+months<-formatC(1:12,width=2,flag=0)
+nmonths<-paste0("m",months)
 
-gsub("(.{2})", "\\1 ",res$season)
+mtable<-do.call("rbind",lapply(strsplit(gsub("(.{2})", "\\1 ",res$season)," "),function(i){as.integer(months%in%i)}))
+mtable<-as.data.frame(mtable)
+names(mtable)<-nmonths
 
+slot(res,"data")<-cbind(res@data,mtable)
 
+boxcut<-bbox2pol(c(460826,2406881,4478758,5945517),proj4string=proj4string(res))
+plot(gIntersection(boxcut,res[res$id=="k50",],byid=TRUE),col=alpha("red",0.15),border=NA)
 
 
