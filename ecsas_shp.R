2786af0712d2f89bfe8f74ef9b43e6591c9e985c
frousseu <francoisrousseu@hotmail.com>
2017-03-04 15:23:53 -0500

    added SOMEC data

--- ecsas_shp.R
+++ ecsas_shp.R
@@ -26,18 +26,33 @@ ll<-"+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 prj<-"+proj=utm +zone=18 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
 laea<-"+proj=laea +lat_0=50 +lon_0=-65"
 
-db<-odbcConnectAccess("D:/ebird/Master ECSAS v 3.51.mdb")
-obs<-sqlFetch(db,"tblSighting",as.is=TRUE)
-sp<-sqlFetch(db,"tblSpeciesInfo",as.is=TRUE)
-watch<-sqlFetch(db,"tblWatch",as.is=TRUE)
-odbcClose(db)
-
-load("D:/ebird/odbcECSAS.RData") #load session instead of running on 32bit all the time
+### get ECSAS data
+#db<-odbcConnectAccess("D:/ebird/Master ECSAS v 3.51.mdb")
+#obs<-sqlFetch(db,"tblSighting",as.is=TRUE)
+#sp<-sqlFetch(db,"tblSpeciesInfo",as.is=TRUE)
+#watch<-sqlFetch(db,"tblWatch",as.is=TRUE)
+#odbcClose(db)
+load("D:/ebird/odbcECSAS.RData") #load session instead of running odbcConnect on 32bit all the time
 rm(kde2pol)
 
-obs$Alpha<-sp$Alpha[match(obs$SpecInfoID,sp$SpecInfoID)]
-obs$sp<-sp$English[match(obs$SpecInfoID,sp$SpecInfoID)]
-obs$Class<-sp$Class[match(obs$SpecInfoID,sp$SpecInfoID)]
+
+### get SOMEC data
+#db<-odbcConnectAccess2007("D:/ebird/SOMEC_CopieMOD31012017_clean.accdb")
+load("D:/ebird/SOMECexport2011andover.RData") #load session instead of running on 32bit all the time
+addQC$WatchID<-paste0("qc",addQC$WatchID)
+addQC$Date<-substr(addQC$"Start Date",1,10)
+watchQC<-addQC[!duplicated(addQC$WatchID),c("WatchID","Date","LongStart","LatStart")]
+obsQC<-addQC[!addQC$Alpha%in%c("",NA),]
+
+
+### join ECSAS and SOMEC data
+watch<-join(watch,watchQC,type="full")
+obs<-join(obs,obsQC,type="full")
+
+obs$Alpha<-NA
+obs$Alpha<-ifelse(is.na(obs$Alpha),sp$Alpha[match(obs$SpecInfoID,sp$SpecInfoID)],obs$Alpha)
+obs$sp<-sp$English[match(obs$Alpha,sp$Alpha)]
+obs$Class<-sp$Class[match(obs$Alpha,sp$Alpha)]
 obs<-obs[obs$Class=="Bird",]
 m<-match(obs$WatchID,watch$WatchID)
 obs$date<-substr(watch$Date[m],1,10)
@@ -48,13 +63,14 @@ obs$lon<-ifelse(!is.na(obs$ObsLong),obs$ObsLong,obs$lon)
 obs$lat<-ifelse(obs$lat<0,-obs$lat,obs$lat)
 obs$lon<-ifelse(obs$lon>0,-obs$lon,obs$lon)
 
-obs<-obs[obs$lon>=(-100) & obs$lon<=30,]
+obs<-obs[obs$lon>=(-80) & obs$lon<=30,]
 obs<-obs[obs$lat>=(40) & obs$lat<=75,]
 
 obs$month<-substr(obs$date,6,7)
 obs$year<-substr(obs$date,1,4)
 obs<-obs[obs$year>="2000",]
 obs$nb<-obs$Count
+obs$nb<-ifelse(is.na(obs$nb),1,obs$nb)
 
 #month_comb<-unlist(list("12010203"=c("12","01","02","03"),"04050607"=c("04","05","06","07"),"08091011"=c("08","09","10","11")))
 #names(month_comb)<-substr(names(month_comb),1,8)
@@ -68,6 +84,7 @@ g<-unique(g[,c("sp","group_kernels")])
 obs$group<-g$group_kernels[match(obs$sp,g$sp)]
 obs$season<-get_season(obs)
 
+
 ### keep only on water obs for waterfowl
 nofly<-c("waterfowl_diving","waterfowl_dabbling")
 w<-which(obs$group%in%nofly & obs$FlySwim=="F")
@@ -75,9 +92,12 @@ obs<-obs[-w,]
 
 x<-obs[,c("sp","group","season","date","month","year","lat","lon","nb")]
 
+
 #################################################################    
 ### get seasonal effort and evaluate at each observation location
 #################################################################
+
+
 
 watch<-watch[substr(watch$Date,1,4)>="2000",]
 watch$month<-substr(watch$Date,6,7)
@@ -137,8 +157,7 @@ for(j in seq_len(nrow(case))){
 	#H[H>0]<-min(H[H>0]) #this thing assumes the same variability in both directions (isotropic) and imposes the smallest value
 	#H1<-H*matrix(c(0.02,0,0,0.02),nrow=2) 
 	H1<-matrix(c(200000000,0,0,200000000),nrow=2) 
-	
-	### GET KERNELS POLYGONS
+	### GET KERNELS POLYGON
 	k<-kde(x=coordinates(xs),binned=FALSE,gridsize=c(500,500),compute.cont=TRUE,H=H1,w=f(xs$nb)*xs$we)
 	kp<-list()
 	perc<-c(25,50,75,95)
@@ -157,6 +176,8 @@ for(j in seq_len(nrow(case))){
 	plot(kp,add=TRUE,col=alpha(cols_kern,0.6),border=NA)
 	plot(land,col="grey95",border="grey75",add=TRUE)
 	legend("bottomright",fill=alpha(cols_kern,0.6),legend=paste(perc,"%"),border=NA,cex=1,bg="grey90",box.lwd=NA,inset=c(0.05,0.1),title="Kernel Contours")
+	info<-paste0("group: ",group,"\nseason: ",season,"\ndata source: ECSAS","\ncoast buffer:")
+	mtext(info,side=3,line=-4,font=2,adj=0.05)
 	
 	dev.off()
 	
